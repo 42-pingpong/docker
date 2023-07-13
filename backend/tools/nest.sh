a77b1836c15580beef14f0bf2c9148d4fe7403e3
myukang <myunghwan0421@gmail.com>
2023-07-14 03:16:09 +0900

    fix: branch 변경되는 문제, local에서 수동으로 바꾸도록

--- backend/tools/nest.sh
+++ backend/tools/nest.sh
@@ -4,6 +4,5 @@ cd /var
 
 git clone https://github.com/42-pingpong/backend
 cd backend
-git checkout develop
 npm install
 npm run start:dev
--- frontend/tools/react.sh
+++ frontend/tools/react.sh
@@ -5,6 +5,5 @@ cd /var
 git clone https://github.com/42-pingpong/frontend.git
 
 cd frontend
-git checkout develop
 npm install
 npm run start
